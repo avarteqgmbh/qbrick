5cd9809d61f1cb38487f59801ef1fd223490bfbd
arkanoryn <arkanoryn@gmail.com>
2016-09-13 09:07:31 +0200

    (HotFix) - add access_level method to admin
    Signed-off-by: arkanoryn <arkanoryn@gmail.com>

--- app/models/qbrick/admin.rb
+++ app/models/qbrick/admin.rb
@@ -6,5 +6,9 @@ module Qbrick
     # :confirmable, :lockable, :timeoutable and :omniauthable
     devise :database_authenticatable,
            :recoverable, :rememberable, :trackable, :validatable
+
+    def has_access_level?(access_lvl)
+      self.access_level == access_lvl
+    end
   end
 end
